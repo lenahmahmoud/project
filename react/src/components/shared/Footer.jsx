49401221f9597a040eb006994e54ee0941c30b1e
lenah <lenah.mahmoud@gmail.com>
2025-10-05 21:58:32 +0300

    api

--- react/src/components/shared/Footer.jsx
+++ react/src/components/shared/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from '../../assets/images/Logo Brand.png';
+import logo from '../../../public/images/Logo Brand.png';
 
 function Footer() {
   return (
